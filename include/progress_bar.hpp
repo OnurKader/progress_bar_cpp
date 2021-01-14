2af8eb95e5327ec7477fc9484f6d35f520a5805e
Onur Orkun KADER <33760623+OnurKader@users.noreply.github.com>
2021-01-14 15:16:00 +0300

    Update progress_bar.hpp
    Remove <iostream> and whitespace changes

--- include/progress_bar.hpp
+++ include/progress_bar.hpp
@@ -1,23 +1,21 @@
 #pragma once
-#include <iostream>
 #include <vector>
 
 class progressBar
 {
-	public:
-		/* Takes in a char for filling up the bar and the size fo the bar */
-		progressBar(char notDoneChar, char doneChar, unsigned int size);
-		void end();
-		/* Fills the bar upto a given number */
-		void fillUpCells(unsigned int cells);
-		/* Fills the bar one by one */
-		void fillUp();
-	private:
-		unsigned int size = 0;
-		unsigned int pos = 1;
-		char c;
-		char ch;
-		std::vector <char> bar;
+public:
+	// Takes in a char for filling up the bar and the size fo the bar
+	progressBar(char notDoneChar, char doneChar, unsigned int size);
+	void end();
+	// Fills the bar upto a given number
+	void fillUpCells(unsigned int cells);
+	// Fills the bar one by one
+	void fillUp();
+
+private:
+	unsigned int size = 0;
+	unsigned int pos = 1;
+	char c;
+	char ch;
+	std::vector<char> bar;
 };
-
-
